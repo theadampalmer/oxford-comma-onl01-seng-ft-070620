0a21a01c695bce3885cb50c0334d771fafd21d90
Adam Palmer <theadampalmer@gmail.com>
2020-07-13 14:40:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  binding.pry
+  if array.length == 1
+     array[0]
+  elsif array.length == 2
+     array.join(' and ')
+  elsif array.length == 3
+    array.join(', ') 
+    array.insert(-2,' and')
+  else 
+    array.join(',')
+    array.insert(-1,' and')
+    array.join(',')
+  end
 end

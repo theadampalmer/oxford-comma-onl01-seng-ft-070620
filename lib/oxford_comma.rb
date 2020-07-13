f258dd2160e63c7934a9642180228c03838ec9d9
Adam Palmer <theadampalmer@gmail.com>
2020-07-13 15:31:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  # binding.pry
+  if array.length == 1
+     array[0]
+  elsif array.length == 2
+     array.join(' and ')
+  elsif array.length >= 3
+  # binding.pry
+    array.last.insert(0, 'and ')
+    array.join(', ') 
+  end
 end

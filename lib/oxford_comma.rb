6139c96443505d9cf2f127a52fa96ac19a7e64bf
Adam Palmer <theadampalmer@gmail.com>
2020-07-13 14:51:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 2
      array.join(' and ')
   elsif array.length == 3
-    array.join(', ') 
+    array.join(',') 
     array.insert(-2,' and')
   else 
     array.join(',')
